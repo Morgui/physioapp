b6731895463ab8a444514733799163f49e6e6824
Luz Divina Delgado Castañeda <luzdivdelg@gmail.com>
2020-03-05 13:18:46 +0100

    fixed env files

---
+++ server/public/precache-manifest.ace77ddd5a38990101f4dce6ba69e1ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae4184db34901b3af65d17336593a05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caa3ad67ddbf1754b3d6",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a4d7aea961e39853f858",
+    "url": "/static/css/main.e825289d.chunk.css"
+  },
+  {
+    "revision": "caa3ad67ddbf1754b3d6",
+    "url": "/static/js/2.2ceaa642.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.2ceaa642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4d7aea961e39853f858",
+    "url": "/static/js/main.a4c69b8b.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
